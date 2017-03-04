a4b7b8c36b55255723832376a061f183d306c222
Raphael <antnraphael@gmail.com>
2017-03-04 12:41:16 -0300

    pasta javascript

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Acesso Restrito</title>
+  </head>
+  <body>
+    <?php
+      include 'login/form.login.php';
+    ?>
+  </body>
+</html>
---
+++ login/form.login.php
@@ -0,0 +1,25 @@
+<div class="">
+  <form class="" onsubmit="return doLogin();">
+    <div class="">
+      <label for="user">Usuário</label>
+      <input type="text" name="user" id="user" />
+    </div>
+
+    <div class="">
+      <label for="passwd">Senha</label>
+      <input type="password" name="passwd" id="passwd" />
+    </div>
+
+    <div class="">
+      <div class="">
+        <button type="submit" name="button" id="button">Autenticar</button>
+      </div>
+
+      <div class="">
+        Esqueceu sua senha
+      </div>
+
+    </div>
+
+</form>
+</div>
