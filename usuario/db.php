074dba7b3ce5cb258e11a81204fe4017130ad588
Raphael <antnraphael@gmail.com>
2017-03-22 18:14:14 -0300

    Centralizando os módulos em diretórios

--- usuario/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-  function conDB()
-  {
-      $con = new PDO('mysql:host=localhost; dbname=produto', 'root', '#armc2014',
-      array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
-      $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      return $con;
-  }
